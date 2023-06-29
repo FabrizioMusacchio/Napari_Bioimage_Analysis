98d0cd465646be94c56cf6c5faabe27f4f8b87ce
Fabrizio Musacchio <fabrizio.musacchio@posteo.de>
2023-06-29 23:38:48 +0200

    updated two python scripts.

--- python_scripts/image_filter.py
+++ python_scripts/image_filter.py
@@ -9,7 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import data
 from skimage.util import random_noise
-from scipy.ndimage import median_filter, gaussian_filter, uniform_filter
+from scipy.ndimage import median_filter, gaussian_filter, uniform_filter, convolve
+from math import exp, pi
 # %% LOAD IMAGE
 # Load sample image from scikit-image
 image = data.camera()
@@ -54,4 +55,136 @@ axes[2].set_title('Gaussian Filtered Image')
 
 plt.tight_layout()
 plt.show()
+# %% DEMONSTRATE FILTER KERNELS
+# Create filter kernels:
+def create_identity_kernel():
+    return np.array([[0, 0, 0],
+                     [0, 1, 0],
+                     [0, 0, 0]])
+
+# define a 5x5 mean filter kernel:
+def create_mean_kernel_5x5():
+    return np.array([[1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1]]) / 25
+    
+
+def create_ridge_detection_kernel():
+    return np.array([[0, 1, 0],
+                     [1, -4, 1],
+                     [0, 1, 0]])
+
+def create_edge_detection_kernel():
+    return np.array([[-1, -1, -1],
+                     [-1, 8, -1],
+                     [-1, -1, -1]])
+
+def create_sharpen_kernel():
+    return np.array([[0, -1, 0],
+                     [-1, 5, -1],
+                     [0, -1, 0]])
+    
+def create_sharpen_kernel_5x5():
+    return np.array([[-1, -1, -1, -1, -1],
+                        [-1, 2, 2, 2, -1],
+                        [-1, 2, 8, 2, -1],
+                        [-1, 2, 2, 2, -1],
+                        [-1, -1, -1, -1, -1]])
+
+def create_mean_kernel():
+    return np.array([[1, 1, 1],
+                     [1, 1, 1],
+                     [1, 1, 1]]) / 9
+
+def create_gaussian_blur_kernel(size=3, sigma=1):
+    kernel = np.fromfunction(lambda x, y: (1 / (2 * np.pi * sigma**2)) * np.exp(-((x - size//2)**2 + (y - size//2)**2) / (2 * sigma**2)), (size, size))
+    return kernel / np.sum(kernel)
+
+def create_gaussian_blur_kernel_formula(size=3, sigma=1):
+    kernel = np.zeros((size, size))
+    center = size // 2
+    for x in range(size):
+        for y in range(size):
+            exponent = -((x - center)**2 + (y - center)**2) / (2 * sigma**2)
+            kernel[x, y] = (1 / (2 * pi * sigma**2)) * exp(exponent)
+    return kernel / np.sum(kernel)
+
+def create_unsharp_masking_kernel(size=5, sigma=1, amount=1):
+    kernel = create_gaussian_blur_kernel(size, sigma)
+    sharpen_kernel = create_sharpen_kernel_5x5()
+    mask = kernel - (amount * sharpen_kernel)
+    return mask
+
+def create_unsharp_masking_kernel_2():
+    return np.array([
+                    [-1, -4, -6, -4, -1],
+                    [-4, -16, -24, -16, -4],
+                    [-6, -24, 476, -24, -6],
+                    [-4, -16, -24, -16, -4],
+                    [-1, -4, -6, -4, -1]
+                ]) / -256
+
+
+def apply_filter(image, kernel):
+    # Pad the image to handle filter border effects
+    padded_image = np.pad(image, ((1, 1), (1, 1)), mode='constant')
+
+    # Apply the filter
+    filtered_image = convolve(padded_image, kernel)
+
+    # Clip values to ensure they are within the valid range
+    filtered_image = np.clip(filtered_image, 0, 255)
+
+    return filtered_image
+
+def apply_median_filter(image, size=3):
+    return median_filter(image, size=size)
+
+# Load the camera image
+image = data.camera()
+image_array = np.array(image)
+
+# Apply filters
+identity_kernel = create_identity_kernel()
+ridge_detection_kernel = create_ridge_detection_kernel()
+edge_detection_kernel = create_edge_detection_kernel()
+sharpen_kernel = create_sharpen_kernel()
+mean_kernel_3x3 = create_mean_kernel()
+mean_kernel_5x5 = create_mean_kernel_5x5()
+gaussian_blur_kernel_3x3 = create_gaussian_blur_kernel(size=3)
+gaussian_blur_kernel_3x3_2 = create_gaussian_blur_kernel_formula(size=3)
+gaussian_blur_kernel_5x5 = create_gaussian_blur_kernel(size=5)
+unsharp_masking_kernel = create_unsharp_masking_kernel(size=5, sigma=1, amount=1)
+unsharp_masking_kernel_2 = create_unsharp_masking_kernel_2()
+median_filtered_image_3x3 = apply_median_filter(image_array, size=3)
+median_filtered_image_5x5 = apply_median_filter(image_array, size=5)
+
+filtered_images = [
+    ("Identity", apply_filter(image_array, identity_kernel)),
+    ("Ridge Detection", apply_filter(image_array, ridge_detection_kernel)),
+    ("Edge Detection", apply_filter(image_array, edge_detection_kernel)),
+    ("Sharpen", apply_filter(image_array, sharpen_kernel)),
+    ("Mean (3x3)", apply_filter(image_array, mean_kernel_3x3)),
+    ("Mean (5x5)", apply_filter(image_array, mean_kernel_5x5)),
+    ("Gaussian Blur (3x3)", apply_filter(image_array, gaussian_blur_kernel_3x3)),
+    ("Gaussian Blur (5x5)", apply_filter(image_array, gaussian_blur_kernel_5x5)),
+    ("Unsharp Masking (variant)", apply_filter(image_array, unsharp_masking_kernel)),
+    ("Unsharp Masking", apply_filter(image_array, unsharp_masking_kernel_2)),
+    ("Median Filter (3x3)", median_filtered_image_3x3),
+    ("Median Filter (5x5)", median_filtered_image_5x5)
+]
+
+# Plot and save filtered images
+for name, filtered_image in filtered_images:
+    plt.figure(figsize=(8, 8))
+    plt.imshow(filtered_image, cmap='gray')
+    plt.title(name, fontsize=28)
+    plt.axis('off')
+    plt.savefig(f"{name.lower().replace(' ', '_')}_filtered.png", bbox_inches='tight', pad_inches=0)
+    plt.show()
+
+
+
 # %% END
--- python_scripts/image_thresholding_comparison.py
+++ python_scripts/image_thresholding_comparison.py
@@ -8,7 +8,7 @@ date: June 22, 2023
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import data
-from skimage.filters import try_all_threshold
+from skimage.filters import try_all_threshold, threshold_otsu
 
 # %% LOAD IMAGE
 cells_image = data.cells3d()
@@ -31,4 +31,52 @@ plt.show()
 
 fig, ax = try_all_threshold(nuclei_2D_projection_noisy, figsize=(5, 10), verbose=True)
 plt.tight_layout()
+plt.show()
+# %% 
+
+
+# Create the noise array
+noise = np.random.random((50, 50))
+
+# Create the square in the center with values 100 times larger
+center = 10
+size = 30
+square = np.zeros((50, 50))
+square[center:center+size, center:center+size] = 2 * noise[center:center+size, center:center+size]
+
+# Combine the noise and square arrays
+image = noise + square
+
+# Flatten the image to 1D
+image_flat = image.flatten()
+
+# Calculate Otsu threshold
+thresh = threshold_otsu(image_flat)
+
+# Plot the image
+plt.figure(figsize=(5, 5))
+plt.imshow(image, cmap='gray')
+plt.title('Image')
+plt.axis('off')
+plt.show()
+
+# Plot the histogram of the image
+plt.figure(figsize=(5, 5))
+plt.hist(image.ravel(), bins=256, color='gray', alpha=0.7)
+plt.axvline(x=thresh, color='r', linestyle='--', linewidth=2, label='Otsu Threshold')
+plt.legend()
+plt.title('Histogram')
+plt.xlabel('Pixel Value')
+plt.ylabel('Frequency')
+plt.show()
+
+
+# Plot the 1D image with the Otsu threshold
+plt.figure(figsize=(5, 5))
+plt.plot(image_flat, color='gray', label='1D-flattened image')
+plt.axhline(y=thresh, color='r', linestyle='--', linewidth=2, label='Otsu Threshold')
+plt.legend()
+plt.title('1D Image with Otsu Threshold')
+plt.xlabel('Pixel Index')
+plt.ylabel('Pixel Value')
 plt.show()
